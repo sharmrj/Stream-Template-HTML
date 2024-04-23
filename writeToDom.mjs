a88cf62870ff5fef4c1f7a25d2f3d8159ce03554
Raghav Sharma <raghavs@adobe.com>
2024-04-23 15:21:32 +0530

    added support for the declarative shadow dom

--- writeToDom.mjs
+++ writeToDom.mjs
@@ -7,25 +7,36 @@ const spanstyle = () => {
 };
 
 const soup = async () => { 
-    const style = await spanstyle()
     return new Promise((resolve) => {
-        setTimeout(() => resolve(html`${style}<span> sooup </span>`), 3000);
+        setTimeout(() => resolve(html`${spanstyle}<span slot='food'>soup</span>`), 3000);
     });
 }
 
 const hungry = () => {
-    return html`<div>I'm very hungry</div>`;
+    return html`<div slot='hungry'>I'm very hungry</div>`;
 };
 
 const f = () => {
     return html`
-        <div>I'd really like some ${soup}<div>
+        <template shadowrootmode='open'>
+            <div>I'd really like some <slot name='food'></slot><div>
+            <slot name="hungry"></slot>
+        </template>
         ${hungry}
+        ${soup}
         `;
 };
 
+const applyDSD = (template) => {
+    const mode = template.getAttribute('shadowrootmode');
+    if (!mode) return;
+    const shadowRoot = template.parentElement.attachShadow({ mode });
+    shadowRoot.appendChild(template.content.cloneNode(true));
+    template.remove();
+};
+
 // TODO: sanitize HTML
-export const streamToDom = async (p, xs) => {
+export const streamToDom = async (p, xs, dsd = false) => {
     const doc = document.implementation.createHTMLDocument();
     // It's safe to use this in spite of the HTML spec warning
     // (https://html.spec.whatwg.org/multipage/dynamic-markup-insertion.html#document.write())
@@ -33,10 +44,22 @@ export const streamToDom = async (p, xs) => {
     // while the [[parser is still running]].
     // This is perfectly fine since we're doing write on a detached document.
     doc.write('<div>');
-    p.appendChild(doc.body.firstChild);
+    const root = doc.body.firstChild;
+    p.appendChild(root);
+    const observer = new MutationObserver((records) => {
+        for (const record of records) {
+            for (const node of record.addedNodes) {
+                if (node instanceof HTMLElement && node.tagName === 'TEMPLATE')
+                    applyDSD(node);
+            }
+        }
+    });
+    if (dsd) observer.observe(root, { childList: true, subtree: true });
     for await (const x of xs) {
       doc.write(x);
     }
+    if (dsd) observer.disconnect();
+
 };
 
 export const test = async (xs) => {
@@ -46,4 +69,4 @@ export const test = async (xs) => {
 };
 
 
-// streamToDom(document.body, f());
+streamToDom(document.body, f(), true);
